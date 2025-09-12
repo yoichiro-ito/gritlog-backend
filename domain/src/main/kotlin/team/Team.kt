459cab99b69f3f247fdb1f48dac42468d6f9d9e2
yoichiro.ito <f3f4khtus.y8160@gmail.com>
2025-09-13 02:27:04 +0900

    team domain

--- domain/src/main/kotlin/team/Team.kt
+++ domain/src/main/kotlin/team/Team.kt
@@ -1,7 +1,13 @@
 package app.gritlog.team
 
 import app.gritlog.DomainEntity
+import app.gritlog.user.Sex
 
 class Team(
-    override var id: Long?
+    override var id: Long?,
+    val teamName: String,
+    val sex: Sex,
+    val teamCategory: TeamCategory,
+    val representiveId: Long,
+    val playersCount: Int,
 ) : DomainEntity<Long>(id)
