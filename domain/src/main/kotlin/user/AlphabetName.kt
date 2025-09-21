d6b5b3dc8c0feb15263ff2ab4a8a4a2f33651fb6
yoichiro.ito <f3f4khtus.y8160@gmail.com>
2025-09-22 03:34:35 +0900

    バリデーションのコメントを追加

--- domain/src/main/kotlin/user/AlphabetName.kt
+++ domain/src/main/kotlin/user/AlphabetName.kt
@@ -2,4 +2,8 @@ package app.gritlog.user
 
 import app.gritlog.ValueObject
 
-data class AlphabetName(val value: String): ValueObject
+data class AlphabetName(val value: String): ValueObject {
+    init {
+        // 半角英数字、半角スペースのみ許可
+    }
+}
--- domain/src/main/kotlin/user/KanaName.kt
+++ domain/src/main/kotlin/user/KanaName.kt
@@ -2,4 +2,8 @@ package app.gritlog.user
 
 import app.gritlog.ValueObject
 
-data class KanaName(val value: String): ValueObject
+data class KanaName(val value: String): ValueObject {
+    init {
+        // 全角カタカナ、半角スペース、全角スペースのみ許可
+    }
+}
